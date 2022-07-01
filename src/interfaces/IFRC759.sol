3e50409ff37043777a3703c7739422961b4956b7
Paddy Curé <paddy.cure@gmail.com>
2022-07-01 18:51:59 +0100

    added some key functions to interface

--- src/interfaces/IFRC759.sol
+++ src/interfaces/IFRC759.sol
@@ -12,6 +12,9 @@ interface IFRC759 {
     function totalSupply() external view returns (uint256);
     function maxSupply() external view returns (uint256);
 
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function timeSliceTransfer(address recipient, uint256 amount, uint256 start, uint256 end) external returns (bool);
+
     function createSlice(uint256 start, uint256 end) external returns (address);
     function sliceByTime(uint256 amount, uint256 sliceTime) external;
     function mergeSlices(uint256 amount, address[] calldata slices) external;
